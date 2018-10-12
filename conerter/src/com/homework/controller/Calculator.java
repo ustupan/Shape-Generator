e5d741c8c86577b553f14c5c9fd1faf6abfe64e1
Jodowy <jodowy@gmail.com>
2018-10-12 14:16:45 +0200

    Structure.

---
+++ conerter/src/com/homework/controller/Calculator.java
@@ -0,0 +1,15 @@
+package com.homework.controller;
+import com.homework.model.*;
+
+
+public class Calculator {
+    private CurrRepository currencyRepository;
+
+    public Calculator(CurrRepository currencyRepository) {
+        this.currencyRepository = currencyRepository;
+    }
+
+    public double calculate(double amount, Currency inputCurrency, Currency outputCurrency){
+        //algorytm
+    }
+}
---
+++ conerter/src/com/homework/controller/DigitController.java
@@ -0,0 +1,9 @@
+package com.homework.controller;
+
+public class DigitController implements ControllerInterface {
+
+    @Override
+    public void check(String x) {
+        /*w przypadku złych danych*/
+    }
+}
